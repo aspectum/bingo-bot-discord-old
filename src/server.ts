732cbeeeb19a415aa3e3b0c02661148efc0a1b87
Pedro Garcia <drogpe@gmail.com>
2021-09-02 10:14:11 -0300

    Connecting with DB and testing DIscord OAuth

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import express from 'express';
+import mongoose from 'mongoose';
+
+import dbConfig from './config/db';
+import auth from './routes/auth';
 
 const PORT = process.env.PORT || 5000;
 
@@ -7,8 +11,17 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/auth', auth);
+
 app.use('/', (req, res, next) => {
     res.send('ok');
+});
+
+mongoose.connect(dbConfig.uri, dbConfig.options);
+
+const db = mongoose.connection;
+db.on('error', () => {
+    throw new Error('Unable to connect to database');
 });
 
 app.listen(PORT, () => {
